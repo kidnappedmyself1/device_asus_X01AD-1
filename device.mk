5d02478dd744cdb18c4ca44c8ffaeb4019b61e86
Rizky Benggolo <travarilo@gmail.com>
2018-10-15 07:42:27 +0000

    X01AD: rootdir: Add Spectrum support
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- device.mk
+++ device.mk
@@ -375,7 +375,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.2 \
-    android.hardware.secure_element@1.0
+    android.hardware.secure_element@1.0 \
+    init.spectrum.rc \
+    init.spectrum.sh
 
 PRODUCT_PACKAGES += \
     librmnetctl \
