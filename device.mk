b27fb02040decf8e5d7ba6a2ca04602b536f9657
APXD-git <63340491+APXD-git@users.noreply.github.com>
2021-01-27 22:00:30 +0530

    X01AD: overlay: Update smart charging nodes for ArrowOS

--- device.mk
+++ device.mk
@@ -13,6 +13,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-arrow \
     $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
