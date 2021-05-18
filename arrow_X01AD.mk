StyxProject-Devices/device_asus_X01AD

X01AD: Initial Arrow OS BringUp

arrow_X01AD.mk
@@ -1,0 +1,37 @@
+#
+# Copyright (C) 2020 ArrowOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/asus/X01AD
+
+# Inherit from source
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Set shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+# Inherit device stuff
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Device identifiers.
+PRODUCT_DEVICE := X01AD
+PRODUCT_NAME := arrow_X01AD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X01AD
+PRODUCT_MANUFACTURER := asus
+TARGET_VENDOR := asus
+BOARD_VENDOR := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="msm8953_64-user 10 WW_Phone-202005071625 17.2018.2004.31-20200507 release-keys"
+BUILD_FINGERPRINT := asus/WW_X01AD/ASUS_X01A_1:10/WW_Phone-202005071625/17.2018.2004.31-20200507:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+# ArrowOS Properties
+TARGET_BOOT_ANIMATION_RES := 720
+DEVICE_MAINTAINER := AP_XD

kidnappedmyself1/device_asus_X01AD-1

arrow_X01AD.mk
@@ -1,0 +1,37 @@
+#
+# Copyright (C) 2020 ArrowOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/asus/X01AD
+
+# Inherit from source
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Set shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+# Inherit device stuff
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Device identifiers.
+PRODUCT_DEVICE := X01AD
+PRODUCT_NAME := arrow_X01AD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X01AD
+PRODUCT_MANUFACTURER := asus
+TARGET_VENDOR := asus
+BOARD_VENDOR := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="msm8953_64-user 10 WW_Phone-202005071625 17.2018.2004.31-20200507 release-keys"
+BUILD_FINGERPRINT := asus/WW_X01AD/ASUS_X01A_1:10/WW_Phone-202005071625/17.2018.2004.31-20200507:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+# ArrowOS Properties
+TARGET_BOOT_ANIMATION_RES := 720
+DEVICE_MAINTAINER := Arka