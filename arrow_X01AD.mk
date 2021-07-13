c139efe9d3c24cbe8e28cc47cf66fe44763d5945
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2021-07-13 16:12:44 +0530

    X01AD: Update to redfin R July fingerprint

--- arrow_X01AD.mk
+++ arrow_X01AD.mk
@@ -30,9 +30,9 @@ BOARD_VENDOR := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_MODEL=ASUS_X01AD \
-    PRIVATE_BUILD_DISC="redfin-user 11 RQ3A.210605.005 7349499 release-keys"
+    PRIVATE_BUILD_DISC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
