a284de79182830957c2865b789179dcf5162b320
kidnappedmyself1 <97450347+kidnappedmyself1@users.noreply.github.com>
2022-01-22 10:29:57 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X01AD.mk
+    $(LOCAL_DIR)/streak_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X01AD-user \
-    aosp_X01AD-userdebug \
-    aosp_X01AD-eng
+    streak_X01AD-user \
+    streak_X01AD-userdebug \
+    streak_X01AD-eng
